8997f11f266c0384fb425acaa3bcd2b907187b13
sbstnkng <koenigse@gmx.de>
2022-06-15 19:28:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      This text is formatted with Tailwind CSS
+    </h1>
+  );
+};
+
+export default App;
