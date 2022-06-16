2dffa76a7e8f5842ac76f20a954e22b5fe6bcabf
sbstnkng <koenigse@gmx.de>
2022-06-16 20:40:57 +0200

    Add container layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      This text is formatted with Tailwind CSS
-    </h1>
+    <div className="container mx-auto flex justify-center">
+      <h1 className="text-3xl font-bold underline">
+        This text is formatted with Tailwind CSS
+      </h1>
+    </div>
   );
 };
 
