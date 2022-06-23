b99ce8faf679fa85966d7fe08f7cc36743d6d3dc
sbstnkng <koenigse@gmx.de>
2022-06-23 18:18:36 +0200

    Add some small styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-semibold text-slate-800 text-center">
-        This page uses Tailwind CSS
+    <div className="container mx-auto flex justify-center">
+      <h1 className="text-3xl font-bold underline">
+        This text is formatted with Tailwind CSS
       </h1>
     </div>
   );
